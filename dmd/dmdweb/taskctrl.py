b8fa99e0fc1f5c901e1eaaf21724b201f321191c
heysion@deepin <heysion@users.noreply.github.com>
2017-04-20 16:38:13 +0800

    add newtask  Changes to be committed: 	modified:   dmd/dmdweb/taskctrl.py 	modified:   dmd/dmdweb/templates/task.html 	new file:   dmd/settings.py

--- dmd/dmdweb/taskctrl.py
+++ dmd/dmdweb/taskctrl.py
@@ -6,13 +6,24 @@
 @copyright: 2017, Heysion Yuan <heysions@gmail.com>
 @license: GPLv3
 '''
+import pdb
+import os
+import shutil
 import time
+import json
 import functools
 import tornado.gen
 from tornado.concurrent import run_on_executor
 from concurrent.futures import ThreadPoolExecutor
+from peewee import SqliteDatabase
 
 from dmd.dmdweb import WebBase
+from dab.db.models import MkisoInfo
+from dmd import settings
+#FIXME
+print("#FIXME")
+db = SqliteDatabase("../peewee.db")
+MkisoInfo._meta.database = db
 
 class TaskNew(WebBase):
     _thread_pool = ThreadPoolExecutor(5)
@@ -33,15 +44,62 @@ class TaskNew(WebBase):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def post(self):
-        tornado.ioloop.IOLoop.instance().add_callback(functools.partial(self.call_mkiso,"abc"))
-        self.render("task.html")
+        req_data = { k: self.get_argument(k) for k in self.request.arguments }
+        if not ("tag" in req_data.keys()):
+            req_data["tag"] = "15.1"
+        if not ("isoname" in req_data  and req_data["isoname"] is not None) :
+            req_data["isoname"] = "deepin-server"
+        tornado.ioloop.IOLoop.instance().add_callback(functools.partial(self.call_mkiso,req_data))
+        print(req_data)
+        self.write(req_data)
+        self.write(self.request.body)
+        #self.render("task.html")
 
     @tornado.concurrent.run_on_executor(executor='_thread_pool')
     def call_mkiso(self,data):
+        task_instance = self.save_task(data)
+        if task_instance :
+            self.init_task_work(task_instance)
+        print("call work")
         time.sleep(5)
-        print("over!")
-        pass
-    pass
+
+    def init_task_work(self,task):
+        task_work_path = "{}/{}".format(settings.DMD_PATH,task.id)
+        if not os.path.exists(task_work_path):
+            os.mkdir(task_work_path)
+        if task.includelist:
+            with open('{}/include.list'.format(task_work_path), 'a') as the_file:
+                the_file.write(task.includelist)
+        if task.excludelist:
+            with open('{}/exclude.list'.format(task_work_path), 'a') as the_file:
+                the_file.write(task.excludelist)
+        with open('{}/config.json'.format(task_work_path), 'a') as the_file:
+            config_data = {"name":task.isoname,
+                           "tag":"15.1",
+                           "arch":"mips64el",
+                           "task":task.id,
+                           "preseed":"preseed.cfg",
+                           "include":"include.list",
+                           "exclude":"exclude.list",
+                           "output":settings.DMD_OUTPUT}
+            the_file.write(json.dumps(config_data))
+        preseed_orig = "{}/{}".format(settings.DMD_UPLOAD,task.preseed_config)
+        if os.path.exists(preseed_orig):
+            shutil.move(preseed_orig,"{}/preseed.cfg".format(task_work_path))
+
+    def save_task(self,data):
+        task = MkisoInfo.select(MkisoInfo.isoname).where(MkisoInfo.isoname==data["isoname"])
+        if not task :
+            task = MkisoInfo.create(isoname=data["isoname"],
+                                        preseed_config=data["preseedfile"],
+                                        includelist=data["includelist"],
+                                        excludelist=data["excludelist"],
+                                        status=100)
+            task.save()
+        else:
+            return None
+        return task
+        
 
 class TaskInfo(WebBase):
     pass
---
+++ dmd/settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2017-04-20
+@author: Heysion Yuan
+@copyright: 2017, Heysion Yuan <heysions@gmail.com>
+@license: GPLv3
+'''
+DMD_PATH="/opt/work"
+DMD_OUTPUT="/opt/work/output"
+DMD_UPLOAD="/opt/work/uploads"
+
